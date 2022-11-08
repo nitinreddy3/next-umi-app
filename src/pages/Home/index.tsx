aaff38839d342579bbf0d6bb498cc5e69b5193b1
nitin reddy <reddynitin3@gmail.com>
2022-11-08 23:47:46 +0530

    Updated the page title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default function Page() {
   return (
     <div>
-      <h1 className={styles.title}>Page Home/index</h1>
+      <h1 className={styles.title}>Home</h1>
     </div>
   );
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default function Page() {
   return (
     <div>
-      <h1 className={styles.title}>Page Login/index</h1>
+      <h1 className={styles.title}>Login</h1>
     </div>
   );
 }
